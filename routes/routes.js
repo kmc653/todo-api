16abb27fa6a798065dedbcb4199876682a5a8712
Kevin Chang <changkaimin.big@gmail.com>
2016-06-15 23:01:01 +0800

    Add DELETE todos route

--- routes/routes.js
+++ routes/routes.js
@@ -45,5 +45,18 @@ module.exports = function (express, app) {
 		res.json(body);
 	});
 	
+	//	DELETE /todos/:id
+	router.delete('/todos/:id', function (req, res) {
+		var todoId = parseInt(req.params.id, 10);
+		var matchedTodo = _.findWhere(todos, {id: todoId});
+		
+		if(matchedTodo) {
+			todos = _.without(todos, matchedTodo);
+			res.json(matchedTodo);
+		} else {
+			res.status(404).json({"error": "no todo found with that id"});
+		}
+	});
+	
 	app.use('/', router);
 }
