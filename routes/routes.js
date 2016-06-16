8a304674209643357c10bc4498446d88bd2572ab
Kevin Chang <changkaimin.big@gmail.com>
2016-06-16 11:01:58 +0800

    Final static todos

--- routes/routes.js
+++ routes/routes.js
@@ -96,6 +96,12 @@ module.exports = function (express, app) {
 			filteredTodos = _.where(filteredTodos, {completed: false});
 		}
 		
+		if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+			filteredTodos = _.filter(filteredTodos, function (todo) {
+				return todo.description.toLowerCase().indexOf(queryParams.q) > -1;
+			});
+		}
+		
 		res.json(filteredTodos);
 	});
 	
